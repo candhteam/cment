8127f867223607320316ac8d5a1981607c2ba69f
Jayshri <team@codeandhue.com>
2022-11-24 12:32:46 +0530

    first changes

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,1 +1,14 @@
-console.log("welcome Code and hue");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $(".navbar").css("background", "#fff");
+      $(".navlinks").css("color", "#000");
+      $(".navbar").css("box-shadow", "0px 4px 7px #77777745");
+    } else {
+      $(".navbar").css("background", "transparent");
+      $(".navlinks").css("color", "#fff");
+      $(".navbar").css("box-shadow", "0px 4px 7px #77777700");
+    }
+  });
+});
